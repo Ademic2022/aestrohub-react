9fb06faf0a3a56f23b185e10f0f8f47e78d1fe49
ademic2022 <adeeyomichael77@gmail.com>
2024-03-11 15:01:55 +0100

    new file:   src/components/data/companies.js

---
+++ src/components/data/companies.js
@@ -0,0 +1,27 @@
+export const companies = [
+  {
+    id: 1,
+    name: "AestroService",
+    logo: "/icons/Saturn.png",
+  },
+  {
+    id: 2,
+    name: "AestroBlog",
+    logo: "/icons/Jupiter.png",
+  },
+  {
+    id: 3,
+    name: "AestroPad",
+    logo: "/icons/Saturn.png",
+  },
+  {
+    id: 4,
+    name: "AestroLaunch",
+    logo: "/icons/Mars.png",
+  },
+  {
+    id: 5,
+    name: "AestroLaunch",
+    logo: "/icons/Mars.png",
+  },
+];
