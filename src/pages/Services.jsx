8cf46284580049c7830da93d3a0e0d511a4a872f
“Ademic” <adeeyomichael77@gmail.com>
2024-03-14 18:43:47 +0100

    modified:   src/pages/Services.jsx

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { Box } from "@mui/material";
-import Section1 from "../components/serviceComponents/Section1";
+import Section1 from "../components/serviceComponents/Home";
+import Marketing from "../components/serviceComponents/Marketing";
+import Design from "../components/serviceComponents/Design";
+import Management from "../components/serviceComponents/Management";
+import Development from "../components/serviceComponents/Development";
+import Writing from "../components/serviceComponents/Writing";
+import Offering from "../components/serviceComponents/Offering";
+import Company from "../components/serviceComponents/Company";
 
 const Services = () => {
   return (
     <React.Fragment>
       <Box mt={5}>
         <Section1 />
+        <Marketing />
+        <Design />
+        <Management />
+        <Development />
+        <Writing />
+        <Offering />
+        <Company />
       </Box>
     </React.Fragment>
   );
