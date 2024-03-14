4268341a0f10d68f6131c8df5c6c9285b7647f88
“Ademic” <adeeyomichael77@gmail.com>
2024-03-14 18:41:43 +0100

    modified:   src/components/data/services.js

--- src/components/data/services.js
+++ src/components/data/services.js
@@ -35,3 +35,108 @@ export const services = [
       "Elevate your product to reach your target audience through a management and marketing strategy that separates you from the crowd.",
   },
 ];
+
+export const marketing = [
+  {
+    id: 1,
+    title: "Paid Marketing",
+    img: "/icons/pill.png",
+    desc: "Elevate your brand's online presence with targeted campaigns across Google Ads, Facebook Ads, and more, driving traffic and boosting conversions.",
+  },
+  {
+    id: 2,
+    title: "Social Media Marketing",
+    img: "/icons/pill.png",
+    desc: "Leverage influencer partnerships to amplify your brand's voice across social platforms through compelling content and fostering community engagement.",
+  },
+  {
+    id: 3,
+    title: "SEO and Content Marketing",
+    img: "/icons/pill.png",
+    desc: "Boost your website's visibility  with strategic keyword research, on-page optimization, content creation, and effective link-building strategies.",
+  },
+];
+
+export const designs = [
+  {
+    id: 1,
+    title: "Branding & Graphic Design",
+    img: "/icons/cube.png",
+    desc: "From logo designs to motion graphics and others, we help you to shape your brand identity and elevate your brand's presence.",
+  },
+  {
+    id: 2,
+    title: "Product Design",
+    img: "/icons/cube.png",
+    desc: "Transform your ideas into innovative and user-centric products that meet the needs of your target market, blending functionality with aesthetic.",
+  },
+  {
+    id: 3,
+    title: "Art",
+    img: "/icons/cube.png",
+    desc: "Express your creativity and vision through various art forms, including illustrations, 2D/3D, digital art, and more, to evoke emotion and tell a story.",
+  },
+];
+
+export const managements = [
+  {
+    id: 1,
+    title: "Project & Product Management",
+    img: "/icons/pyramid.png",
+    desc: "We drive successful project outcomes and product launches with effective planning, execution, and team coordination. Try us today...",
+  },
+  {
+    id: 2,
+    title: "Community Management",
+    img: "/icons/pyramid.png",
+    desc: "We cultivate, moderate, and engage with your community to build brand loyalty, gather feedback, and foster meaningful connections.",
+  },
+  {
+    id: 3,
+    title: "Operations Management",
+    img: "/icons/pyramid.png",
+    desc: "We streamline business operations and optimize processes to improve efficiency, reduce costs, and enhance overall performance.",
+  },
+];
+
+export const developments = [
+  {
+    id: 1,
+    title: "Business Development",
+    img: "/icons/mask.png",
+    desc: "Drive growth and expand your business with strategic planning, market analysis, and partnership building to maximize opportunities and increase revenue.",
+  },
+  {
+    id: 2,
+    title: "Product Development",
+    img: "/icons/mask.png",
+    desc: "Drive growth and expand your business with strategic planning, market analysis, and partnership building to maximize opportunities and increase revenue.",
+  },
+  {
+    id: 3,
+    title: "Blockchain Development",
+    img: "/icons/mask.png",
+    desc: "Develop decentralized applications (DApps), smart contracts, or secure digital wallets with our team of experts at your finger tips.",
+  },
+];
+
+export const writings = [
+  {
+    id: 1,
+    title: "Copy Writing",
+    img: "/icons/helix.png",
+    desc: "Craft compelling and persuasive copy that resonates with your audience and drives action, advertisements, websites, or marketing material.",
+  },
+  {
+    id: 2,
+    title: "Articles and Blog Posts",
+    img: "/icons/helix.png",
+    desc: "Create engaging and informative articles that educate, entertain, and inspire your readers, establishing your authority and building a loyal audience.",
+  },
+  {
+    id: 3,
+    title: "Whitepapers",
+    img: "/icons/helix.png",
+    desc: "We dive deep into complex topics, providing in-depth analysis, insights, and solutions that showcase your expertise and thought leadership in your industry.",
+  },
+];
