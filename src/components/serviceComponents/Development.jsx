abe35162c0000dc8931938f416e4722008ed5e7e
“Ademic” <adeeyomichael77@gmail.com>
2024-03-14 18:42:22 +0100

    new file:   src/components/serviceComponents/Development.jsx

---
+++ src/components/serviceComponents/Development.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import Card from "../common/Card";
+import { developments } from "../data/services";
+
+const Development = () => {
+  return (
+    <React.Fragment>
+      <Box
+        textAlign="center"
+        width={{ xs: "100%", md: "65%", margin: "0 auto", textAlign: "center" }}
+        mt={15}
+        mb={5}
+      >
+        <Typography
+          mb={3}
+          variant="h2"
+          sx={{ fontSize: { xs: "32px", md: "56px" } }}
+        >
+          Development
+        </Typography>
+      </Box>
+      <Card datas={developments} />
+    </React.Fragment>
+  );
+};
+
+export default Development;
