58846a202e2ea66c87f2d61021ca1d7c9e449536
“Ademic” <adeeyomichael77@gmail.com>
2024-03-14 18:42:53 +0100

    new file:   src/components/serviceComponents/Management.jsx

---
+++ src/components/serviceComponents/Management.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import Card from "../common/Card";
+import { managements } from "../data/services";
+
+const Management = () => {
+  return (
+    <React.Fragment>
+      <Box
+        textAlign="center"
+        width={{ xs: "100%", md: "65%", margin: "0 auto", textAlign: "center" }}
+        mt={15}
+        mb={5}
+      >
+        <Typography
+          mb={3}
+          variant="h2"
+          sx={{ fontSize: { xs: "32px", md: "56px" } }}
+        >
+          Management
+        </Typography>
+      </Box>
+      <Card datas={managements} />
+    </React.Fragment>
+  );
+};
+
+export default Management;
