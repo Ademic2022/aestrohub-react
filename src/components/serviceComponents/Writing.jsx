340d176288df0ba80470e1a08a8d8e76c82bc814
“Ademic” <adeeyomichael77@gmail.com>
2024-03-14 18:43:38 +0100

    new file:   src/components/serviceComponents/Writing.jsx

---
+++ src/components/serviceComponents/Writing.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import Card from "../common/Card";
+import { writings } from "../data/services";
+
+const Writing = () => {
+  return (
+    <React.Fragment>
+      <Box
+        textAlign="center"
+        width={{ xs: "100%", md: "65%", margin: "0 auto", textAlign: "center" }}
+        mt={15}
+        mb={5}
+      >
+        <Typography
+          mb={3}
+          variant="h2"
+          sx={{ fontSize: { xs: "32px", md: "56px" } }}
+        >
+          Writing
+        </Typography>
+      </Box>
+      <Card datas={writings} />
+    </React.Fragment>
+  );
+};
+
+export default Writing;
