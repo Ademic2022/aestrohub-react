c430ee3d7d9fa1f0411078a3f60a8a5c08694f57
“Ademic” <adeeyomichael77@gmail.com>
2024-03-16 00:36:02 +0100

    modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeToggleProvider>
       <CssBaseline>
-        <Router>
+        <Router basename="/">
           <App />
         </Router>
       </CssBaseline>
